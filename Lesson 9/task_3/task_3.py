0e224acf9bc74824ee4609123bf1ed848f9a8de6
Andrey Moiseenko(lessons) <130937758+balllagurochka@users.noreply.github.com>
2024-12-10 15:03:10 +0300

    Update task_3.py
    hueta

--- Lesson 9/task_3/task_3.py
+++ Lesson 9/task_3/task_3.py
@@ -1,30 +1,27 @@
 import re
 
 #Поиск слов и их количества
-def most_count(a):
-    #Ищем слова и помещаем их в список
+def get_most_count(a):
     words = re.findall(r'\b\w+\b', a[0])
-    #Словарь где key= слово value= количество
-    word_counts = {}
+    #key= слово value= количество
+    word_counts = {} # 
     for word in words:
         word_counts[word] = word_counts.get(word, 0) + 1 #
-        most_word = max(word_counts, key=word_counts.get)
-        count = word_counts[most_word]
+        most_word = max(word_counts, key=word_counts.get())
+        count = word_counts.get(most_word)
     return most_word, count
 
-#Открываем файл
+#-------
 def file_open(a):
     with open(f"{a}", "r", encoding="utf-8") as file:
-        #Добавляем строки в список
         list_of_str = file.readlines()
-    print(list_of_str)
-    result = []
-    #Каждая строка в виде списка
+    results = []
     for str_file in list_of_str:
         most_word, count = most_count([str_file])
-        result.append((most_word, count))
-    return result
+        results.append((most_word, count))
+    return results
 
+#---------
 def new(file_path):
     result = file_open(file_path)
     with open("New_file.txt", "w", encoding="utf-8") as new_file:
